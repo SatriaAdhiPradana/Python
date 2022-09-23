93f9b85ea21d20a1c313769bbe5fff47e62a1e5c
Satria Adhi Pradana <92844616+SatriaAdhiPradana@users.noreply.github.com>
2022-09-23 16:28:53 +0700

    Create __init__.py

---
+++ face recognition/face_recognition/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__author__ = """Satria Adhi Pradana"""
+__email__ = 'satriaadhipradana0@gmail.com'
+__version__ = '1.4.0'
+
+from .api import load_image_file, face_locations, batch_face_locations, face_landmarks, face_encodings, compare_faces, face_distance
